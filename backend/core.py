5e5fef2cd1c6ec8dcd8793320f7e234375581bbb
Areeb Shafqat <areeb.s@acs.com.sa>
2024-12-04 16:51:32 +0300

    fine tuned prompt

--- backend/core.py
+++ backend/core.py
@@ -154,7 +154,7 @@ def run_llm(query: str, chat_history, chat, docsearch, domain=None):
         domain_retriever = docsearch.as_retriever(
             search_kwargs={
                 "filter": {},  # No filter applied for global search.
-                "k": 5  # Retrieve top 7 results.
+                "k": 6  # Retrieve top 7 results.
             }
         )
     else:
@@ -162,7 +162,7 @@ def run_llm(query: str, chat_history, chat, docsearch, domain=None):
         domain_retriever = docsearch.as_retriever(
             search_kwargs={
                 "filter": {"domain": domain},  # Apply domain-specific filter.
-                "k": 5  # Retrieve top 10 results.
+                "k": 10  # Retrieve top 10 results.
             }
         )
         
